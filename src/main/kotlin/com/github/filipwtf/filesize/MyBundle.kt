43c13dc5947ec6538b9e0e05332b18315ec7dcc5
GitHub Action <action@github.com>
2020-09-23 19:30:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/filipwtf/filesize/MyBundle.kt
+++ src/main/kotlin/com/github/filipwtf/filesize/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.filipwtf.filesize
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/filipwtf/filesize/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/filipwtf/filesize/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.filipwtf.filesize.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.filipwtf.filesize.MyBundle
 
 class MyApplicationService {
 
